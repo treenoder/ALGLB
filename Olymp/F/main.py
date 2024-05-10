2a2abb43c3f8952e9d99e43d1b5297d7688838bc
Senior <senior@dev.lan>
2024-05-10 16:24:59 +0300

    Olymp F partial

--- Olymp/F/main.py
+++ Olymp/F/main.py
@@ -1,5 +1,29 @@
+SI = {
+    "tera": 12,
+    "giga": 9,
+    "mega": 6,
+    "kilo": 3,
+    "deci": -1,
+    "centi": -2,
+    "milli": -3,
+    "micro": -6,
+    "nano": -9,
+}
+
+
 def solution(length: str) -> int:
-    return 0
+    if length == "meter":
+        return 0
+    convert_power = 0
+    for prefix, power in SI.items():
+        if length.startswith(prefix):
+            convert_power = power + solution(length[len(prefix):])
+            break
+
+    if "^" in length:
+        unit = int(length.split("^")[1])
+        return convert_power * unit
+    return convert_power
 
 
 def main():
@@ -8,5 +32,5 @@ def main():
     print(result)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
