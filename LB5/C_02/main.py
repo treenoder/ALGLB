aa177e217d74316755cc7736328069df058874a6
asymp <asymptotelian@gmail.com>
2024-05-15 22:34:40 +0300

    LB5 C-02 partial pypy

--- LB5/C_02/main.py
+++ LB5/C_02/main.py
@@ -1,30 +1,33 @@
-def solution(n, arr) -> tuple[int, list[int]]:
+def solution(n, arr):
     dist = [float('inf')] * n
     pred = [-1] * n
-    dist[0] = 0
-
-    for _ in range(n - 1):
-        for u in range(n):
-            for v in range(n):
-                if arr[u][v] != 100000 and dist[u] + arr[u][v] < dist[v]:
-                    dist[v] = dist[u] + arr[u][v]
-                    pred[v] = u
+    for src in range(n):
+        dist[src] = 0
+        for _ in range(n - 1):
+            for u in range(n):
+                for v in range(n):
+                    if arr[u][v] != 100000 and dist[u] + arr[u][v] < dist[v]:
+                        dist[v] = dist[u] + arr[u][v]
+                        pred[v] = u
 
     for u in range(n):
         for v in range(n):
             if arr[u][v] != 100000 and dist[u] + arr[u][v] < dist[v]:
-                cycle = []
+                x = v
                 for _ in range(n):
-                    v = pred[v]
+                    x = pred[x]
 
-                cycle_start = v
-                visited = set()
-                while cycle_start not in visited:
-                    visited.add(cycle_start)
+                cycle_start = x
+                cycle = []
+                while True:
                     cycle.append(cycle_start + 1)
                     cycle_start = pred[cycle_start]
+                    if cycle_start + 1 in cycle:
+                        cycle.append(cycle_start + 1)
+                        break
 
-                cycle = cycle[cycle.index(cycle_start + 1):]
+                cycle.reverse()
+                cycle = cycle[cycle.index(cycle_start + 1):-1]
 
                 return len(cycle), cycle
 
