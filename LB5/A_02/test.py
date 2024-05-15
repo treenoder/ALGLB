76684fcc61f80cef12006a8aa55150d512139447
Senior <senior@dev.lan>
2024-05-15 14:29:43 +0300

    LB5 A-02 partial

--- LB5/A_02/test.py
+++ LB5/A_02/test.py
@@ -3,7 +3,7 @@ from LB5.A_02.main import solution
 
 def test_solution():
     table = [
-        ([1, 10, 2, 15], [
+        (4, [1, 10, 2, 15], [
             [1, 2],
             [1, 3],
             [4, 2],
@@ -11,5 +11,5 @@ def test_solution():
         ], 2)
     ]
 
-    for costs, roads, expected in table:
-        assert solution(costs, roads) == expected
+    for n, costs, roads, expected in table:
+        assert solution(n, costs, roads) == expected
