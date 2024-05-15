5a88748fb2f37dba8ad6af138469f4eb35346bde
Senior <senior@dev.lan>
2024-05-15 12:29:53 +0300

    LB5 A-02 init

---
+++ LB5/A_02/test.py
@@ -0,0 +1,15 @@
+from LB5.A_02.main import solution
+
+
+def test_solution():
+    table = [
+        ([1, 10, 2, 15], [
+            [1, 2],
+            [1, 3],
+            [4, 2],
+            [4, 2],
+        ], 2)
+    ]
+
+    for narr, marr, expected in table:
+        assert solution(narr, marr) == expected
