4dba627271b45eb6a9396200517d328324cc566c
asymp <asymptotelian@gmail.com>
2024-05-15 21:33:13 +0300

    LB5 C

---
+++ LB5/C/test.py
@@ -0,0 +1,16 @@
+from LB5.C.main import solution
+
+
+def test_solution():
+    table = [
+        (4, 5, [
+            [1, 2, 10],
+            [2, 3, 10],
+            [1, 3, 100],
+            [3, 1, -10],
+            [2, 3, 1],
+        ], [0, 10, 11, 30000])
+    ]
+
+    for n, m, arr, expected in table:
+        assert solution(n, m, arr) == expected
