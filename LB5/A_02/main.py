5cc7699e9203a009efb2ab65503fd502c4ed6c45
Senior <senior@dev.lan>
2024-05-15 13:10:01 +0300

    LB5 A-02 partial

--- LB5/A_02/main.py
+++ LB5/A_02/main.py
@@ -1,5 +1,41 @@
+import heapq
+
+
 def solution(costs, roads):
-    ...
+    n = len(costs)
+
+    graph = [[] for _ in range(n)]
+    for a, b in roads:
+        graph[a - 1].append((b - 1, costs[a - 1]))
+        graph[b - 1].append((a - 1, costs[b - 1]))
+
+    dist = [float('inf')] * n
+    dist[0] = 0
+    jerrycan = costs[0]
+
+    pq = [(0, 0)]
+    while pq:
+        d, u = heapq.heappop(pq)
+
+        if dist[u] < d:
+            continue
+
+        for v, c in graph[u]:
+            new_d = d + c
+            if new_d < dist[v]:
+                dist[v] = new_d
+                heapq.heappush(pq, (new_d, v))
+
+            new_d_with_refuel = d + min(c, jerrycan)
+            if new_d_with_refuel < dist[v]:
+                dist[v] = new_d_with_refuel
+                heapq.heappush(pq, (new_d_with_refuel, v))
+
+                jerrycan -= min(c, jerrycan)
+                if u != 0:
+                    jerrycan = costs[u]
+
+    return dist[n - 1] if dist[n - 1] != float('inf') else -1
 
 
 def main():
