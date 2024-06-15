c63f0ee97c898b3a0a207ca7f70361b3cd850cb7
Senior <senior@dev.lan>
2024-06-15 12:49:02 +0300

    sorting

--- LB2/D_02/main.py
+++ LB2/D_02/main.py
@@ -1,9 +1,40 @@
+def reverse_merge_sort(arr, start, end, di):
+    if start >= end:
+        return
+
+    mid = start + (end - start) // 2
+
+    reverse_merge_sort(arr, start, mid, di)
+    reverse_merge_sort(arr, mid + 1, end, di)
+
+    i, j = start, mid + 1
+    temp = []
+    while i <= mid and j <= end:
+        if arr[i][di] >= arr[j][di]:
+            temp.append(arr[i])
+            i += 1
+        else:
+            temp.append(arr[j])
+            j += 1
+
+    while i <= mid:
+        temp.append(arr[i])
+        i += 1
+
+    while j <= end:
+        temp.append(arr[j])
+        j += 1
+
+    for i in range(len(temp)):
+        arr[start + i] = temp[i]
+
+
 def solution(records, queries):
     results = []
 
     for ci, di in queries:
-        sorted_records = sorted(records, key=lambda x: x[di-1], reverse=True)
-        results.append(sorted_records[ci-1])
+        reverse_merge_sort(records, 0, len(records) - 1, di - 1)
+        results.append(records[ci - 1])
 
     return results
 
